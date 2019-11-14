73ae95d022a9f5819515b76d4e66e5cfe822d5e4
sieder villareal <sieder00@gmail.com>
2019-11-14 14:23:09 +0300

    Updates

---
+++ precache-manifest.98fde652bbe7b100ae37dca995ffb8f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d296b4e76d0dae81f8bc2b35a95da5d",
+    "url": "/react-demo-test/index.html"
+  },
+  {
+    "revision": "39aa7829c551e12afe76",
+    "url": "/react-demo-test/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "4ce3120344e6105ad174",
+    "url": "/react-demo-test/static/css/main.c2ce5ee2.chunk.css"
+  },
+  {
+    "revision": "39aa7829c551e12afe76",
+    "url": "/react-demo-test/static/js/2.1316c078.chunk.js"
+  },
+  {
+    "revision": "4ce3120344e6105ad174",
+    "url": "/react-demo-test/static/js/main.5403157b.chunk.js"
+  },
+  {
+    "revision": "bdb16724d593ad98954e",
+    "url": "/react-demo-test/static/js/runtime-main.c6a4a050.js"
+  }
+]);
